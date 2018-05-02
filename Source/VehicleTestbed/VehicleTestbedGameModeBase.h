a78415f9f5f90880b3ed0eb25ebee4565fa860e2
Jacob T Carroll <jacobtcarroll13@gmail.com>
2018-05-02 21:04:41 +1000

    Initial commit

---
+++ Source/VehicleTestbed/VehicleTestbedGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VehicleTestbedGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VEHICLETESTBED_API AVehicleTestbedGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};

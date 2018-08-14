6afd6b16f91dffe011d9172c934b9bf91fcd7339
Matthew Rimkevicus <7678487@student.swin.edu.au>
2018-08-14 18:27:16 +1000

    Extended the event recording so that we can record whatever details we want with whatever xml tags we want.  Using TMaps to link key-value pairs such that the key is the xml tag and the value is the description.  This is also open to blueprints.

--- Source/VehicleTestbed/EventRecorder/EventRecorder.cpp
+++ Source/VehicleTestbed/EventRecorder/EventRecorder.cpp
@@ -9,9 +9,14 @@ std::atomic<bool> UEventRecorder::bPause = false;
 std::atomic<bool> UEventRecorder::bStop = false;
 std::condition_variable UEventRecorder::C;
 
-void UEventRecorder::AddEventToQueue(const FString aName, const FString aHandler)
+void UEventRecorder::RecordEvent(const FString aName, const FString aHandler)
 {
-	FRecordableEvent REvent = FRecordableEvent(aName, aHandler);
+	RecordEventWithDetails(aName, aHandler, TMap<FString, FString>());
+}
+
+void UEventRecorder::RecordEventWithDetails(const FString aName, const FString aHandler, const TMap<FString, FString> details)
+{
+	FRecordableEvent REvent = FRecordableEvent(aName, aHandler, details);
 	QueueMutex.lock();
 	WriteQueue.push(REvent);
 	QueueMutex.unlock();
@@ -61,8 +66,14 @@ void UEventRecorder::Write()
 
 			CheckForPause();  // check for a pause before and after writing
 
-			const FString Line = FString("<event>\r\n\t<name>") + REvent.Name + FString("</name>\r\n\t<time>")
-			+ REvent.Timestamp + FString("</time>\r\n\t<handler>") + REvent.Handler + FString("</handler>\r\n</event>\r\n");
+			FString Line = FString("<event>\r\n\t<time>") + REvent.Timestamp + FString("</name>\r\n\t<name>")
+				+ REvent.Name + FString("</time>\r\n\t<handler>") + REvent.Handler + FString("</handler>\r\n");
+
+			for (FRecordableEvent::Pair pair : REvent.Details)
+			{
+				Line.Append(FString("\t<") + pair.Key + FString(">") + pair.Value + FString("</") + pair.Key + FString(">\r\n"));
+			}
+			Line.Append(FString("</event>\r\n"));
 
 			// This crazy method automatically handles opening, closing and creating the file
 			FFileHelper::SaveStringToFile(Line, *FileName, FFileHelper::EEncodingOptions::AutoDetect, &IFileManager::Get(), EFileWrite::FILEWRITE_Append);
@@ -85,8 +96,3 @@ void UEventRecorder::CheckForPause()
 		C.wait(lock);
 	}
 }
-
-FRecordableEvent::FRecordableEvent(const FString aName, const FString aHandler)
-	: Name(aName), Handler(aHandler),
-	Timestamp(FDateTime::Now().ToString(*FString("%d/%m/%Y - %H:%M:%S:%s")))
-{ }
--- Source/VehicleTestbed/EventRecorder/EventRecorder.h
+++ Source/VehicleTestbed/EventRecorder/EventRecorder.h
@@ -10,12 +10,12 @@
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "EventRecorder.generated.h"
 
-///<summary>Collects details of events and writes them to a file</summary>
 UCLASS()
+///<summary>Collects details of events and writes them to a file</summary>
 class VEHICLETESTBED_API UEventRecorder : public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY()
-	
+
 	// variables
 private:
 	static std::queue<struct FRecordableEvent> WriteQueue;
@@ -31,24 +31,34 @@ private:
 
 	// functions
 public:
+	UFUNCTION(BlueprintCallable, Category = "EventRecorder")
+	///<summary>Creates a thread running <see cref="Write()" /></summary>
+	static void Start();
+
+	UFUNCTION(BlueprintCallable, Category = "EventRecorder")
+	///<summary>Sets the pause variable to true, causing the WriterThread to pause when it next checks the boolean's value</summary>
+	static void Pause();
+
+	UFUNCTION(BlueprintCallable, Category = "EventRecorder")
+	///<summary>Sets the pause variable to false, causing the waiting WriterThread to unpause</summary>
+	static void Resume();
+
+	UFUNCTION(BlueprintCallable, Category = "EventRecorder")
+	///<summary>Sets the stop variable to true, causing the WriterThread to complete gracefully when it next checks the boolean's value</summary>
+	static void Stop();
+
+	UFUNCTION(BlueprintCallable, Category = "EventRecorder")
 	///<summary>Collects data about an event and adds it to the queue to be written</summary>
 	///<param name="aName">Name of the event which was triggered</param>
 	///<param name="aHandler">Name of the object which is handling the event</param>
-	UFUNCTION(BlueprintCallable, Category="EventRecorder")
-	static void AddEventToQueue(const FString aName, const FString aHandler);
+	static void RecordEvent(const FString aName, const FString aHandler);
 
-	///<summary>Creates a thread running <see cref="Write()" /></summary>
-	UFUNCTION(BlueprintCallable, Category="EventRecorder")
-	static void Start();
-	///<summary>Sets the pause variable to true, causing the WriterThread to pause when it next checks the boolean's value</summary>
 	UFUNCTION(BlueprintCallable, Category = "EventRecorder")
-	static void Pause();
-	///<summary>Sets the pause variable to false, causing the waiting WriterThread to unpause</summary>
-	UFUNCTION(BlueprintCallable, Category = "EventRecorder")
-	static void Resume();
-	///<summary>Sets the stop variable to true, causing the WriterThread to complete gracefully when it next checks the boolean's value</summary>
-	UFUNCTION(BlueprintCallable, Category="EventRecorder")
-	static void Stop();
+	///<summary>Collects data about an event and adds it to the queue to be written</summary>
+	///<param name="aName">Name of the event which was triggered</param>
+	///<param name="aHandler">Name of the object which is handling the event</param>
+	///<param name="details">A TMap of key-value pairs to log</param>
+	static void RecordEventWithDetails(const FString aName, const FString aHandler, const TMap<FString, FString> details);
 
 private:
 	///<summary>Loop function which is run by the WriterThread.  When the queue of events is not empty it will write their details to a file one by one</summary>
@@ -60,9 +70,17 @@ private:
 ///<summary>A simple structure to hold information about an event trigger</summary>
 typedef struct FRecordableEvent
 {
-	const FString Name;
+public:
+	typedef TPair<FString, FString> Pair;
+
 	const FString Timestamp;
+	const FString Name;
 	const FString Handler;
 
-	FRecordableEvent(const FString aName, const FString aHandler);
+	const TMap<FString, FString> Details;
+
+	FRecordableEvent(const FString aName, const FString aHandler, const TMap<FString, FString> details)
+		: Timestamp(FDateTime::Now().ToString(*FString("%d/%m/%Y - %H:%M:%S.%s"))),
+		Name(aName), Handler(aHandler), Details(details)
+	{}
 };

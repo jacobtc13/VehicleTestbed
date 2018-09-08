3021aff2048179df092f1d3885341318e7cc1f67
Matthew Rimkevicus <7678487@student.swin.edu.au>
2018-09-08 15:18:53 +1000

    - Tracked down and fixed the crashing issue with the pause menu.  Pointer needed to be a uproperty. - Modified the way the pause menu interacts with the spawn menu slightly to resolve null reference warnings in the log files. - Changed all "Get Player Controller" nodes to "Get Owning Player Controller" nodes. - Changed the vehicle speed and orientation widgets to use the TWV functions intead of the sedan ones and modified the speed function to display in km/h instead of cm/s. - Disconnected the Tick event of the display widget in favour of letting the speed and orientation widgets update their own text. - Rearranged the player controller h file to follow the public, protected, private order.

--- Source/VehicleTestbed/PlayerController/TestbedPlayerControllerBase.h
+++ Source/VehicleTestbed/PlayerController/TestbedPlayerControllerBase.h
@@ -21,7 +21,23 @@ public:
 	virtual void BeginPlay() override;
 
 	// Pawn Swapping
+	UFUNCTION(Category="Testbed Wheeled Vehicle", BlueprintCallable)
+	///<summary>Switches to the next available vehicle in the array</summary>
+	void CycleCharacterForward();
+
+	UFUNCTION(Category="Testbed Wheeled Vehicle", BlueprintCallable)
+	///<summary>Switches to the previous available vehicle in the array</summary>
+	void CycleCharacterBackward();
+
+	// Pausing
+	///<summary>Adds the pause widget to the screen and pauses the game</summary>
+	void Pause();
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Widgets")
+	TSubclassOf<UUserWidget> PauseMenu;
+
 private:
+	// Pawn Swapping
 	TArray<AActor*> ControllablePawns;
 
 	int32 CurrentPawnIndex;
@@ -31,23 +47,7 @@ private:
 	///<param name='IsCycleForward'>Is the character being changed to the next one in the list?</param>  
 	void CycleCharacter(bool IsCycleForward);
 
-public:
-	//void SwapPawn();
-	UFUNCTION(Category = "Testbed Wheeled Vehicle", BlueprintCallable)
-	///<summary>Switches to the next available vehicle in the array</summary>
-	void CycleCharacterForward();
-
-	UFUNCTION(Category = "Testbed Wheeled Vehicle", BlueprintCallable)
-	///<summary>Switches to the previous available vehicle in the array</summary>
-	void CycleCharacterBackward();
-
-
 	// Pausing
-public:
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Widgets")
-	TSubclassOf<UUserWidget> PauseMenu;
-private:
+	UPROPERTY()
 	UUserWidget* PauseMenuInstance;
-public:
-	void Pause();
 };

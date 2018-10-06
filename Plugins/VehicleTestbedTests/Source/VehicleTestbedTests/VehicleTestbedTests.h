950e618790ee825aab216130185c96f04c2b3c28
Matthew Rimkevicus <7678487@student.swin.edu.au>
2018-10-06 15:09:49 +1000

    Copied auto-generated comments to the header file

--- Plugins/VehicleTestbedTests/Source/VehicleTestbedTests/VehicleTestbedTests.h
+++ Plugins/VehicleTestbedTests/Source/VehicleTestbedTests/VehicleTestbedTests.h
@@ -10,6 +10,9 @@ class FVehicleTestbedTestsModule : public IModuleInterface
 public:
 
 	/** IModuleInterface implementation */
+	///<summary>This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module</summary>
 	virtual void StartupModule() override;
+
+	///<summary>This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading, we call this function before unloading the module.</summary>
 	virtual void ShutdownModule() override;
 };

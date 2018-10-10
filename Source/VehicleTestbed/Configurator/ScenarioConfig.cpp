9506a8fcec9401809571d42ee25220fd6d46d0a1
Matthew Rimkevicus <7678487@student.swin.edu.au>
2018-10-10 12:42:56 +1100

    Start of the scenario config class

---
+++ Source/VehicleTestbed/Configurator/ScenarioConfig.cpp
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ScenarioConfig.h"
+
+rapidxml::xml_node<>* UScenarioConfig::GetXMLNode()
+{
+	using namespace rapidxml;
+	xml_document<> Doc;
+	xml_node<>* BaseNode = Doc.allocate_node(node_element, "Scenario");
+	for (UAgentConfig* Agent : Agents)
+	{
+		xml_node<>* AgentNode = Doc.allocate_node(node_element, "Agent", TCHAR_TO_UTF8(Agent));
+		BaseNode->append_node(AgentNode);
+	}
+	xml_node<>* MapNode = Doc.allocate_node(node_element, "Map", TCHAR_TO_UTF8(*MapName.ToString()));
+	BaseNode->append_node(MapNode);
+
+	return BaseNode;
+}
---
+++ Source/VehicleTestbed/Configurator/ScenarioConfig.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "AgentConfig.h"
+
+#include "CoreMinimal.h"
+#include "Configurator/ConfigBase.h"
+#include "ScenarioConfig.generated.h"
+
+UCLASS()
+class VEHICLETESTBED_API UScenarioConfig : public UConfigBase
+{
+	GENERATED_BODY()
+	
+public:
+	///<summary>Default Constructor</summary>
+	UScenarioConfig() = default;
+	///<summary>Destructor</summary>
+	~UScenarioConfig() = default;
+	
+	///<summary>Generates a rapidxml node structure from the information in this object</summary>
+	///<returns>A pointer to a rapidxml node structure depicting the information from this object</returns>
+	rapidxml::xml_node<>* GetXMLNode() override;
+
+private:
+	TLinkedList<UAgentConfig*> Agents;
+	// TODO: Spawn points
+	FName MapName;
+};

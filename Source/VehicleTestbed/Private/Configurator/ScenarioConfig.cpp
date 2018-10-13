95740d5d963be46d999ac5c4c5e39e8621532b41
Matthew Rimkevicus <7678487@student.swin.edu.au>
2018-10-13 15:40:16 +1100

    Partial implementation of getters and setters for the ScenarioConfig class

--- Source/VehicleTestbed/Private/Configurator/ScenarioConfig.cpp
+++ Source/VehicleTestbed/Private/Configurator/ScenarioConfig.cpp
@@ -7,9 +7,11 @@ rapidxml::xml_node<>* UScenarioConfig::GetXMLNode()
 	using namespace rapidxml;
 	xml_document<> Doc;
 	xml_node<>* BaseNode = Doc.allocate_node(node_element, "Scenario");
-	for (UAgentConfig* Agent : Agents)
+	TArray<FString> AgentFiles;
+	Agents.GetKeys(AgentFiles);
+	for (FString Agent : AgentFiles)
 	{
-		xml_node<>* AgentNode = Doc.allocate_node(node_element, "Agent", TCHAR_TO_UTF8(Agent));
+		xml_node<>* AgentNode = Doc.allocate_node(node_element, "Agent", TCHAR_TO_UTF8(*Agent));
 		BaseNode->append_node(AgentNode);
 	}
 	xml_node<>* MapNode = Doc.allocate_node(node_element, "Map", TCHAR_TO_UTF8(*MapName.ToString()));
--- Source/VehicleTestbed/Public/Configurator/ScenarioConfig.h
+++ Source/VehicleTestbed/Public/Configurator/ScenarioConfig.h
@@ -21,8 +21,26 @@ public:
 	///<returns>A pointer to a rapidxml node structure depicting the information from this object</returns>
 	rapidxml::xml_node<>* GetXMLNode() override;
 
+	UFUNCTION()
+	///<summary>Returns an array of the file locations of the agent configs used in this scenario</summary>
+	///<returns>An array of file locations of the agents configs used in this scenario</returns>
+	TArray<FString> GetAgentFileLocations() const;
+	UFUNCTION()
+	///<summary>Returns an array of Agent Config objects used by this scenario, creating them from file if they don't exist in memory</summary>
+	///<returns>An array of Agent Config objects used by this scenario</returns>
+	TArray<UAgentConfig*> GetAgents();
+
+	UFUNCTION()
+	///<summary>Returns the name of the map used in this scenario</summmary>
+	///<returns>The name of the map uesd in this scenario</returns>
+	FName GetMapName() const;
+	UFUNCTION()
+	///<summary>Sets the name of the map used for this scenario</summary>
+	///<param="NewMapName">The name of the new map used for this scenario</param>
+	void SetMapName(FName NewMapName);
+
 private:
-	TLinkedList<UAgentConfig*> Agents;
+	TMap<FString, UAgentConfig*> Agents;
 	// TODO: Spawn points
 	FName MapName;
 };

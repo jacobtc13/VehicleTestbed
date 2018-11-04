41a939ff6984c4218874b2b4baa9a54a2c9a77f2
Matthew Rimkevicus <7678487@student.swin.edu.au>
2018-11-04 15:58:50 +1100

    Filled out the xml structure `AppendDocument` function

--- Source/VehicleTestbed/Private/Configurator/CommConfig.cpp
+++ Source/VehicleTestbed/Private/Configurator/CommConfig.cpp
@@ -11,7 +11,24 @@ void UCommConfig::AppendDocument(rapidxml::xml_document<>& OutDocument) const
 	xml_node<>* BaseNode = OutDocument.allocate_node(node_element, "Communication");
 	OutDocument.append_node(BaseNode);
 
+	// Save the default snr model to use
+	xml_node<>* DefaultModelNode = OutDocument.allocate_node(node_element, "DefaultModel", OutDocument.allocate_string(TCHAR_TO_UTF8(*DefaultModelName.ToString())));
+	BaseNode->append_node(DefaultModelNode);
 
+	// Save the list of frequency ranges
+	xml_node<>* FrequencyRangeOuterNode = OutDocument.allocate_node(node_element, "FrequencyRanges");
+	BaseNode->append_node(FrequencyRangeOuterNode);
+	for (const auto& FrequencyRange : FrequencyRanges)
+	{
+		xml_node<>* FrequencyRangeNode = OutDocument.allocate_node(node_element, "FrequencyRange");
+		FrequencyRangeOuterNode->append_node(FrequencyRangeNode);
+		xml_node<>* FrequencyRangeModelNode = OutDocument.allocate_node(node_element, "SNRModel", OutDocument.allocate_string(TCHAR_TO_UTF8(*FrequencyRange.ModelName.ToString())));
+		FrequencyRangeNode->append_node(FrequencyRangeModelNode);
+		xml_node<>* FrequencyRangeMinFreqNode = OutDocument.allocate_node(node_element, "MinFrequency", OutDocument.allocate_string(TCHAR_TO_UTF8(*FString::SanitizeFloat(FrequencyRange.MinFrequency))));
+		FrequencyRangeNode->append_node(FrequencyRangeMinFreqNode);
+		xml_node<>* FrequencyRangeMaxFreqNode = OutDocument.allocate_node(node_element, "MaxFrequency", OutDocument.allocate_string(TCHAR_TO_UTF8(*FString::SanitizeFloat(FrequencyRange.MaxFrequency))));
+		FrequencyRangeNode->append_node(FrequencyRangeMaxFreqNode);
+	}
 }
 
 bool UCommConfig::InitializeFromXML(rapidxml::xml_document<>& Doc)
@@ -103,6 +120,10 @@ void UCommConfig::PopulateSNRModelsArray()
 
 bool UCommConfig::IsClassNameValidSNRModel(const FName& ModelName)
 {
+	if (!SNRModels.Num())
+	{
+		PopulateSNRModelsArray();
+	}
 	for (const auto& Class : SNRModels)
 	{
 		if (ModelName == Class.Get()->GetFName())

1901219510c3e68c78918d6d3914abf4867163fd
Simon Daniel <simonjdaniel@gmail.com>
2018-11-07 22:12:48 +1100

    Amended Rotators on SpawnPoints (Yaw and Roll were switched)

--- Source/VehicleTestbed/Private/SpawnPoints/SpawnPointList.cpp
+++ Source/VehicleTestbed/Private/SpawnPoints/SpawnPointList.cpp
@@ -14,11 +14,11 @@ SpawnPointList::SpawnPointList()
 void SpawnPointList::PopulateList()
 {
 	spList.Add(SpawnPoint(FName(TEXT("DEFAULT")), FVector(0, 0, 0), FRotator(0, 0, 0), ""));
-	spList.Add(SpawnPoint(FName(TEXT("ALLY1")), FVector(128581.210938, 161129.390625, -53666.914062), FRotator(0, 0, 165.599533), ""));
-	spList.Add(SpawnPoint(FName(TEXT("ALLY2")), FVector(179069.40625, 137120, -53797.636719), FRotator(0, 0, 161.999588), ""));
+	spList.Add(SpawnPoint(FName(TEXT("ALLY1")), FVector(128581.210938, 161129.390625, -53666.914062), FRotator(0, 165.599533, 0), ""));
+	spList.Add(SpawnPoint(FName(TEXT("ALLY2")), FVector(179069.40625, 137120, -53797.636719), FRotator(0, 161.999588, 0), ""));
 	spList.Add(SpawnPoint(FName(TEXT("ENEMY1")), FVector(80932.445312, 164541.28125, -47791.210938), FRotator(0, 0, 0), ""));
-	spList.Add(SpawnPoint(FName(TEXT("ENEMY2")), FVector(87885.257812, 205574.890625, -48679.117188), FRotator(0, 0, 295.199066), ""));
-	spList.Add(SpawnPoint(FName(TEXT("ENEMY3")), FVector(114588.4375, 206807.359375, -45675.542969), FRotator(0, 0, 273.59924), ""));
+	spList.Add(SpawnPoint(FName(TEXT("ENEMY2")), FVector(87885.257812, 205574.890625, -48679.117188), FRotator(0, 295.199066, 0), ""));
+	spList.Add(SpawnPoint(FName(TEXT("ENEMY3")), FVector(114588.4375, 206807.359375, -45675.542969), FRotator(0, 273.59924, 0), ""));
 
 
 }
